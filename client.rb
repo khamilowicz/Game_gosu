1177e6d396b1d99b86e168d59a3743fe33348928
khamilowicz <piotr.szeremeta@gmail.com>
2012-12-24 02:07:17 +0100

    Spaceobject dziala, jeszcze pobieranie danych

--- client.rb
+++
@@ -1,9 +0,0 @@
-require "socket"
-
-s = TCPSocket.new 'localhost', 8000
-
-while line = s.gets
-				puts line 
-end
-
-s.close
--- first.rb
+++
@@ -1,8 +0,0 @@
-require 'gosu'
-require "texplay"
-require "./Spaceship.rb"
-require "./Photon.rb"
-require "./Window.rb"
-
-window = SpaceBattle.new
-window.show
